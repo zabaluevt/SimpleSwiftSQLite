51b808a10f555799b089515068a9d3054955b52e
Тимофей Забалуев <zabti@yandex.ru>
2019-03-10 15:09:53 +0400

    Initial Commit

---
+++ TestSQLite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestSQLite
+//
+//  Created by Тимофей Забалуев on 10/03/2019.
+//  Copyright © 2019 Тимофей Забалуев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
